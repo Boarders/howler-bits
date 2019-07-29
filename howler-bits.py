daf0e8d5048a657e94f004ab99e59f6359f31686
Boarders <callan.mcgill@gmail.com>
2019-07-29 12:20:37 -0400

    Take command line args and use proper main

--- howler-bits.py
+++ howler-bits.py
@@ -3,30 +3,21 @@ from helpers import *
 import os
 import sys
 
-audio_dir = 'TODO'
-file_name = "room4.mp3"
-# remove the directory
-base_name = os.path.basename(file_name)
-# split the name and file extension)
-(name, extension) = (os.path.splitext(file_name))
-
-audio = AudioSegment.from_mp3(base_name)
-
-def main_transformation():
-    (seg1, seg2, seg3, third) = split_in_three(audio)
-    segs1 = split_audio_and_make_name (five_min, seg1, name, 0)
-    segs2 = split_audio_and_make_name (ten_min,  seg2, name, third)
-    segs3 = split_audio_and_make_name (fifteen_min, seg2, name, 2 * third)
-
-    output_all(segs1)
-    output_all(segs2)
-    output_all(segs3)
-
-main_transformation()
+def main():
+    cl_inputs = sys.argv[1:]
+    if (len(cl_inputs) == 0):
+        sys.exit ("No input files provided!")
+    else:
+        for file_name in cl_inputs:
+            dir_name = os.path.dirname(file_name)
+            ext = os.path.splitext(file_name)[1][1:]
+            audio = AudioSegment.from_file(file_name, ext)
+            name = os.path.basename(file_name)
+            
+            split_and_output_audio(audio, ext, name, file_name, dir_name)
 
 
-def split_file(file_name):
-    base_name = os.path.basename(file_name)
-    audio = Audio.Segment.from_mp3(file_name)
-    ext = os.path.splitext(file_name)
+if __name__ == "__main__":
+    main()
+
 
